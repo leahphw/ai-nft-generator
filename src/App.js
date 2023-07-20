a63fa1648fb84778973e0bde777cc7a5481f3113
Leah Le <phnganh.le@gmail.com>
2023-07-20 17:58:28 +0700

    init and config

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+import { NFTStorage, File } from 'nft.storage'
+import { Buffer } from 'buffer';
+import { ethers } from 'ethers';
+import axios from 'axios';
+
+// Components
+import Spinner from 'react-bootstrap/Spinner';
+import Navigation from './components/Navigation';
+
+// ABIs
+import NFT from './abis/NFT.json'
+
+// Config
+import config from './config.json';
 
 function App() {
+  const [provider, setProvider] = useState(null)
+  const [account, setAccount] = useState(null)
+
+  const loadBlockchainData = async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    setProvider(provider)
+  }
+
+  useEffect(() => {
+    loadBlockchainData()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navigation account={account} setAccount={setAccount} />
+      <p>Edit App.js to get started.</p>
+
     </div>
   );
 }
 
 export default App;
+
